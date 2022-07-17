f8577ea295c1220ef7cd5c9c5f6c985c7d017552
vladiguez <ylannguez@gmail.com>
2022-07-17 15:10:39 +0000

    fd

--- Velo.py
+++ Velo.py
@@ -61,22 +61,22 @@ st.subheader("Lockin Duration Analysis")
 st.write("Among the 3 main locking periods: short term (from 1 day to 2 weeks), mid term (1 year) and long term (4 years), we observe more volume on the very long term, meaning that LPers have globally a longer term staking strategy ")
 st.plotly_chart(px.scatter(df, y ="VALUE", x ="LOCKIN_DURATION", color="DEPOSIT_TYPE", log_y=True), use_container_width=True)
 
-st.write("OUT FLOWS")
-st.write("all the Assets - USD equivalent - token name and volumes. Those flows could come from swap, farming LDO, CVX, AAVE are well represented." )
-st.plotly_chart(px.scatter(df_Celsius_sushi_crosschain_swaps, y ="AMOUNT_OUT_USD", x ="SYMBOL_OUT", color="BLOCKCHAIN"), use_container_width=True)
-st.plotly_chart(px.scatter(df_Celsius_sushi_crosschain_swaps, y ="AMOUNT_OUT_USD", x ="BLOCK_TIMESTAMP", color="SYMBOL_OUT"), use_container_width=True)
-#######################################
+# st.write("OUT FLOWS")
+# st.write("all the Assets - USD equivalent - token name and volumes. Those flows could come from swap, farming LDO, CVX, AAVE are well represented." )
+# st.plotly_chart(px.scatter(df_Celsius_sushi_crosschain_swaps, y ="AMOUNT_OUT_USD", x ="SYMBOL_OUT", color="BLOCKCHAIN"), use_container_width=True)
+# st.plotly_chart(px.scatter(df_Celsius_sushi_crosschain_swaps, y ="AMOUNT_OUT_USD", x ="BLOCK_TIMESTAMP", color="SYMBOL_OUT"), use_container_width=True)
+# #######################################
 
-st.subheader("CELSIUS SUSHI- MAINNET ACTIVITY ")
-st.write("IN FLOWS")
-st.plotly_chart(px.bar(df_Celsius_sushi_ethereum_swaps, y ="AMOUNT_IN_USD", x ="SYMBOL_IN", color="SYMBOL_IN"), use_container_width=True)
-st.plotly_chart(px.scatter(df_Celsius_sushi_ethereum_swaps, y =["SYMBOL_IN", "AMOUNT_IN_USD"], x ="BLOCK_TIMESTAMP", color="AMOUNT_IN_USD"), use_container_width=True)
-st.write("OUT FLOWS")
-st.plotly_chart(px.bar(df_Celsius_sushi_ethereum_swaps, y ="AMOUNT_OUT_USD", x ="SYMBOL_OUT", color="SYMBOL_OUT"), use_container_width=True)
-st.plotly_chart(px.scatter(df_Celsius_sushi_ethereum_swaps, y =["SYMBOL_OUT", "AMOUNT_OUT_USD"], x ="BLOCK_TIMESTAMP", color="AMOUNT_OUT_USD"), use_container_width=True)
-#######################################
+# st.subheader("CELSIUS SUSHI- MAINNET ACTIVITY ")
+# st.write("IN FLOWS")
+# st.plotly_chart(px.bar(df_Celsius_sushi_ethereum_swaps, y ="AMOUNT_IN_USD", x ="SYMBOL_IN", color="SYMBOL_IN"), use_container_width=True)
+# st.plotly_chart(px.scatter(df_Celsius_sushi_ethereum_swaps, y =["SYMBOL_IN", "AMOUNT_IN_USD"], x ="BLOCK_TIMESTAMP", color="AMOUNT_IN_USD"), use_container_width=True)
+# st.write("OUT FLOWS")
+# st.plotly_chart(px.bar(df_Celsius_sushi_ethereum_swaps, y ="AMOUNT_OUT_USD", x ="SYMBOL_OUT", color="SYMBOL_OUT"), use_container_width=True)
+# st.plotly_chart(px.scatter(df_Celsius_sushi_ethereum_swaps, y =["SYMBOL_OUT", "AMOUNT_OUT_USD"], x ="BLOCK_TIMESTAMP", color="AMOUNT_OUT_USD"), use_container_width=True)
+# #######################################
 
-st.subheader("CELSIUS STAKING activity on SUSHI-ETHEREUM MAINNET")
-st.plotly_chart(px.scatter(df_Celsius_sushi_ethereum_staking, y ="EVENT_NAME", x ="CONTRACT_NAME"), use_container_width=True)
+# st.subheader("CELSIUS STAKING activity on SUSHI-ETHEREUM MAINNET")
+# st.plotly_chart(px.scatter(df_Celsius_sushi_ethereum_staking, y ="EVENT_NAME", x ="CONTRACT_NAME"), use_container_width=True)
 
 #######################################
